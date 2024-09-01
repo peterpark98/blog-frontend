1a9183b25d99a21287dad4029737b8f2d9060825
小彼得 <550755782@qq.com>
2024-09-01 16:59:08 +0800

    add BlogList.vue

--- components.d.ts
+++ components.d.ts
@@ -7,6 +7,7 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
+    BlogList: typeof import('./src/components/BlogList.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     TheCounter: typeof import('./src/components/TheCounter.vue')['default']
---
+++ src/components/BlogList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Blog List</h1>
+    <ul>
+      <li v-for="blog in blogs" :key="blog.id">
+        {{ blog.title }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+const blogs = ref([])
+
+const fetchBlogs = async () => {
+  try {
+    const response = await axios.get('http://127.0.0.1:5000/api/blogs')
+    blogs.value = response.data
+  } catch (error) {
+    console.error('Failed to fetch blogs:', error)
+  }
+}
+
+onMounted(fetchBlogs)
+</script>
